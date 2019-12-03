1c2a09b3ed28c8cd5810f1aac7f2f6c3461a554a
sdmum <54817439+sdmum@users.noreply.github.com>
2019-12-02 20:42:17 -0600

    Project Progression

--- script.js
+++ script.js
@@ -1,26 +1,35 @@
 window.onload = () => {
 
-    let firstPagehtml =
+    let loginTemplate =
         `<h1> Please Login </h1><br>
         Username: <input type="text" value="mwp"></input> <br> 
         Password: <input type="text" value="123"></input> <br>
         <button type="button" id="loginBtn"> Login </button>`
 
-    let animationPagehtml;
+    let animationTemplate =
+        `<br>
+        <textarea rows="40" cols="100" id="animationTextArea"></textarea><br>
+        <button type="button" id="refreshBtn"> Refresh Animation </button> 
+        <button type="button" id="logoutBtn"> Logout </button> `
 
     let isLogin;
     let myObj;
     let animString;
-    // let myToken = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJtd2EiLCJpc3N1ZWRBdCI6IjIwMTktMTEtMjciLCJ1c2VybmFtZSI6Im13cCJ9.U9ciwh5lcPwFlJdxhNQkeiMc7AMYAnawfKNidw8CNDpTIUjNBL_EtDqkXG4qGOF8H_Ve1S2Gg2PwmCYOkfgmWA"
 
-    document.querySelector("#outlet").innerHTML = firstPagehtml
+    // let loginBtn = document.getElementById("loginBtn")
+
+    let refresh;
+
+    document.querySelector("#outlet").innerHTML = loginTemplate
     document.getElementById("loginBtn").addEventListener("click", login)
 
 
 
     function login() {
-        const inputs = document.querySelectorAll('input') // [input, input]
 
+        loginBtn.addEventListener("click", _ => window.history.pushState({ page: 1 }, "title 1", "?page=1"))
+
+        const inputs = document.querySelectorAll('input') // [input, input]
 
         fetch('http://mumstudents.org/api/login', {
             method: 'POST',
@@ -44,7 +53,7 @@ window.onload = () => {
 
     function logOut() {
         if (isLogin) {
-            document.querySelector("#outlet").innerHTML = firstPagehtml
+            document.querySelector("#outlet").innerHTML = loginTemplate
         }
         isLogin = false
     }
@@ -73,27 +82,44 @@ window.onload = () => {
                 city = locationObj.results[0].locations[0].adminArea5
                 state = locationObj.results[0].locations[0].adminArea3
                 country = locationObj.results[0].locations[0].adminArea1
+
+                animationTemplate = `<h2 id=welcomeId> Welcome ${city} ${state} ${country} <h2><br>
+                <textarea rows="40" cols="100" id="animationTextArea"></textarea><br>
+                <button type="button" id="refreshBtn"> Refresh Animation </button> 
+                <button type="button" id="logoutBtn"> Logout </button> `
             })
     }
-
-
 
     function fail(msg) {
         console.log(msg.code + msg.message); // Log the error
     }
 
 
+
+
     function animationFunc() {
 
-        console.log(myObj.token + "");
-        let myToken = `eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJtd2EiLCJpc3N1ZWRBdCI6IjIwMTktMTEtMjciLCJ1c2VybmFtZSI6Im13cCJ9.U9ciwh5lcPwFlJdxhNQkeiMc7AMYAnawfKNidw8CNDpTIUjNBL_EtDqkXG4qGOF8H_Ve1S2Gg2PwmCYOkfgmWA`
 
-        // URL: http://mumstudents.org/api/animation
-        // HTTP verb: GET
-        // Request header must include the following:
-        // Authorization: Bearer token
-        // Replace the token from the response received after you log in.
-        // The server will send back a string response contains the full ASCII animation frames
+        document.querySelector("#outlet").innerHTML = animationTemplate;
+
+        // refresh = document.getElementById("refreshBtn").addEventListener("click", animationFetch)
+
+        animationFetch();
+
+    }
+
+    let i = 0;
+    function playAnim() {
+
+        document.getElementById("animationTextArea").innerHTML = animString[i];
+        ++i
+        if (i === animString.length) {
+            i = 0;
+        }
+
+    }
+
+    function animationFetch() {
         fetch("http://mumstudents.org/api/animation", {
             headers: {
 
@@ -108,40 +134,11 @@ window.onload = () => {
                 animString = animString.split("=====");
                 console.log(animString);
 
-                animationPagehtml = `<h2 id=welcomeId> Welcome ${city} ${state} ${country} <h2><br>
-                <textarea rows="40" cols="100" id="animationTextArea"></textarea><br>
-                <button type="button" id="refreshBtn"> Refresh Animation </button> 
-                <button type="button" id="logoutBtn"> Logout </button> `
-
-                document.querySelector("#outlet").innerHTML = animationPagehtml;
-
-                // document.getElementById("animationTextArea").innerHTML = animString
-
-
-                // let i = 0;
-                // setInterval((animString) => {
-                //     console.log(animString[i])
-                //     i++
-                //     if (i === animString.length - 1) {
-                //         i = 0
-                //     }
-                // }, 200);
-
-
-
-
-                let i = 0
                 setInterval(playAnim, 200);
-
-                function playAnim() {
-                    //console.log(animString[i])
-                    document.getElementById("animationTextArea").innerHTML = animString[i];
-                    ++i
-                    if (i === animString.length - 1) {
-                        i = 0;
-                    }
-                }
             })
     }
+
+
 }
 
+//document.getElementById("refreshBtn").addEventListener("click", animationFetch)
